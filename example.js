30485a8fa8b322c025a5ecace6a07802bf14e6ae
James Pearce <github@tripleodeon.com>
2010-09-27 23:05:16 -0600

    First version of code & documentation

---
+++ example.js
@@ -0,0 +1,14 @@
+var Connect = require("connect"),
+    Monomi = require("monomi");
+
+Connect.createServer(
+
+    Monomi.detectBrowserType(),
+
+    function(request, response, next) {
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.write('Hello World, ');
+        response.end('and thanks for using a ' + request.monomi.browserType + ' browser');
+    }
+
+).listen(8080);
